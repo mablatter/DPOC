6bccc6528e4ef0926b54e03446885b9d8896bbe1
Lucia <luliu@ethz.ch>
2017-11-28 20:40:41 +0100

    Added stage cost computation

--- ComputeStageCosts.m
+++ ComputeStageCosts.m
@@ -60,5 +60,111 @@ function G = ComputeStageCosts( stateSpace, controlSpace, mazeSize, walls, targe
 
 % put your code here
 
+L= size(controlSpace,1);
+MN=size(stateSpace,1);
+G=ones(MN,L);
+
+disturbance=[0 0; 1 0; 1 1; 0 1; -1 1; -1 0; -1 -1; 0 -1; 1 -1];
+Prob_disturb=[1/9 1/9 1/9 1/9 1/9 1/9 1/9 1/9 1/9];
+
+for i = 1 : MN
+	for l = 1 : L
+        not_valid = 0;
+        new_loc=stateSpace(i,:)+controlSpace(l,:)
+        if (new_loc(1) > mazeSize(1) || new_loc(1) < 1 || new_loc(2) > mazeSize(2) || new_loc(2) < 1)
+            G(i,l) = inf;
+            continue;        
+        end
+        distance=max(max(abs(controlSpace(l,:))));
+        end_probability = 1;
+        Prev_Pos = stateSpace(i,:);
+        if distance > 0    
+            step_ball=controlSpace(l,:)./distance;
+            for x = 1:distance
+                if sum(abs(step_ball))==1
+                    if (sum(step_ball)==-1)
+                        wall_coincidence=ismember([Prev_Pos+step_ball; Prev_Pos+step_ball+flip(step_ball)],walls,'rows');
+                    else
+                        wall_coincidence=ismember([Prev_Pos; Prev_Pos-flip(step_ball)],walls,'rows');
+                    end
+                    if sum(wall_coincidence) == 2
+                        G(i,l) = inf;
+                        not_valid = 1;
+                        continue;
+                    end
+                elseif sum(abs(step_ball))>1
+                    if (step_ball(1) == 1)
+                        if (step_ball(2) == 1)
+                            wall_coincidence=ismember([Prev_Pos; Prev_Pos+[0 -1]; Prev_Pos + [-1 0]; Prev_Pos + [0 1]; Prev_Pos + [1 0]],walls,'rows'); 
+                        elseif (step_ball(2) == -1)
+                            wall_coincidence=ismember([Prev_Pos + [0 -1]; Prev_Pos+[0 -2]; Prev_Pos + step_ball; Prev_Pos; Prev_Pos + [-1 -1]],walls,'rows'); 
+                        end
+                    elseif (step_ball(1) == -1)
+                        if (step_ball(2) == 1)
+                            wall_coincidence=ismember([Prev_Pos + [-1 0]; Prev_Pos+[-1 -1]; Prev_Pos + step_ball; Prev_Pos; Prev_Pos + [-2 0]],walls,'rows'); 
+                        elseif (step_ball(2) == -1)
+                            wall_coincidence=ismember([Prev_Pos + step_ball; Prev_Pos+[-1 -2]; Prev_Pos + [-2 -1]; Prev_Pos + [-1 0]; Prev_Pos + [0 -1]],walls,'rows'); 
+                        end
+                    end
+                    if wall_coincidence(1)==1 && sum(wall_coincidence) > 1
+                        G(i, l) = inf;
+                        not_valid = 1;
+                        continue;
+                    end
+                end
+                Prev_Pos = Prev_Pos + step_ball;
+                hole_at_pos = ismember(Prev_Pos, holes, 'rows');
+                if sum(hole_at_pos) > 0
+                    G(i,l) = G(i,l) + end_probability*p_f*c_r;
+                    end_probability = end_probability*(1 - p_f);
+                end
+            end
+            if not_valid
+                G(i,l) = inf;
+                continue;
+            end
+        end
+        for a = 1: size(disturbance(:,1))
+            next_pos = stateSpace(i,:)+controlSpace(l,:)+ disturbance(a,:);
+            if (next_pos(1) > mazeSize(1) || next_pos(1) < 1 || next_pos(2) > mazeSize(2) || next_pos(2) < 1)
+                G(i,l) = G(i,l) + end_probability*Prob_disturb(a)*c_p;
+                continue;        
+            end
+            Prev_Pos = stateSpace(i,:)+controlSpace(l,:);
+            if sum(abs(disturbance(a, :)))==1
+                if (sum(disturbance(a, :))==-1)
+                    wall_coincidence=ismember([Prev_Pos+disturbance(a, :); Prev_Pos+disturbance(a, :)+flip(disturbance(a, :))],walls,'rows');
+                else
+                    wall_coincidence=ismember([Prev_Pos; Prev_Pos-flip(disturbance(a, :))],walls,'rows');
+                end
+                if sum(wall_coincidence) == 2
+                    G(i,l) = G(i,l) + end_probability*Prob_disturb(a)*c_p;
+                    continue;
+                end
+            elseif sum(abs(disturbance(a, :)))>1
+                if (disturbance(a, 1) == 1)
+                    if (disturbance(a, 2) == 1)
+                        wall_coincidence=ismember([Prev_Pos; Prev_Pos+[0 -1]; Prev_Pos + [-1 0]; Prev_Pos + [0 1]; Prev_Pos + [1 0]],walls,'rows'); 
+                    elseif (disturbance(a, 2) == -1)
+                        wall_coincidence=ismember([Prev_Pos + [0 -1]; Prev_Pos+[0 -2]; Prev_Pos + disturbance(a, :); Prev_Pos; Prev_Pos + [-1 -1]],walls,'rows'); 
+                    end
+                elseif (disturbance(a, 1) == -1)
+                    if (disturbance(a, 2) == 1)
+                        wall_coincidence=ismember([Prev_Pos + [-1 0]; Prev_Pos+[-1 -1]; Prev_Pos + disturbance(a, :); Prev_Pos; Prev_Pos + [-2 0]],walls,'rows'); 
+                    elseif (disturbance(a, 2) == -1)
+                        wall_coincidence=ismember([Prev_Pos + disturbance(a, :); Prev_Pos+[-1 -2]; Prev_Pos + [-2 -1]; Prev_Pos + [-1 0]; Prev_Pos + [0 -1]],walls,'rows');
+                    end
+                end
+                if wall_coincidence(1)==1 && sum(wall_coincidence) > 1
+                    G(i,l) = G(i,l) + end_probability*Prob_disturb(a)*c_p;
+                    continue;
+                end
+            end
+            hole_at_pos = ismember(Prev_Pos + disturbance(a, :), holes, 'rows');
+            if sum(hole_at_pos) > 0
+                G(i,l) = G(i,l) + end_probability*p_f*c_r;
+            end
+      end
+   end
 end
 
